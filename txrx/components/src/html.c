f1e5c095da3c03081763d4d0df882abf71f3920b
abidrennan <143449277+abidrennan@users.noreply.github.com>
2025-03-27 17:37:43 -0600

    Update html.c

--- txrx/components/src/html.c
+++ txrx/components/src/html.c
@@ -16,30 +16,34 @@ esp_err_t update_root_html(void) {
 				"<h1>\"MASTER\"</h1>"
 				"<p>\"textbox test\"</p>"
 				"<form action=\"/\" method=\"POST\">"
-					"<label for=\"control_0\">control_0: </label>"
-					"<input type=\"number\" name=\"control_0\" placeholder=%s>"
+					//  changing control0 name
+					"<label for=\"output_ip1\">output_ip1: </label>"
+					"<input type=\"number\" name=\"output_ip1\" placeholder=%s>"
 					"<input type=\"submit\" value=\"submit\"/>"
 				"</form>"
 				"<form action=\"/\" method=\"POST\">"
-					"<label for=\"control_1\">control_1: </label>"
-					"<input type=\"number\" name=\"control_1\" placeholder=%s>"
+					// changing control1 name
+					"<label for=\"output_ip2\">output_ip2: </label>"
+					"<input type=\"number\" name=\"output_ip2\" placeholder=%s>"
 					"<input type=\"submit\" value=\"submit\"/>"
 				"</form>"
 				"<form action=\"/\" method=\"POST\">"
-					"<label for=\"control_2\">control_2: </label>"
-					"<input type=\"number\" name=\"control_2\" placeholder=%s>"
+					// changing control2 name
+					"<label for=\"output_ip3\">output_ip3: </label>"
+					"<input type=\"number\" name=\"output_ip3\" placeholder=%s>"
 					"<input type=\"submit\" value=\"submit\"/>"
 				"</form>"
 				"<form action=\"/\" method=\"POST\">"
-					"<label for=\"control_3\">control_3: </label>"
-					"<input type=\"number\" name=\"control_3\" placeholder=%s>"
+					// changing control3 name
+					"<label for=\"output_ip4\">output_ip4: </label>"
+					"<input type=\"number\" name=\"output_ip4\" placeholder=%s>"
 					"<input type=\"submit\" value=\"submit\"/>"
 				"</form>"
 			"</body>"
 			"</html>",
-			control_0,
-			control_1,
-			control_2,
-			control_3);
+			output_ip1,
+			output_ip2,
+			output_ip3,
+			output_ip4);
 	return err;
 }
