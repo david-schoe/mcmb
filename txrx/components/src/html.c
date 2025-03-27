2aee25cc372642bce3cd59aae5a890f62842d821
abidrennan <143449277+abidrennan@users.noreply.github.com>
2025-03-27 17:53:33 -0600

    Update html.c

--- txrx/components/src/html.c
+++ txrx/components/src/html.c
@@ -7,43 +7,49 @@ char root_html[ROOT_HTML_SZ];
 esp_err_t update_root_html(void) {
 
 	memset(root_html,0,ROOT_HTML_SZ);
-	err=sprintf(root_html,"<!DOCTYPE html>"
-			"<html>"
-			"<head>"
-				"<title>\"MASTER\"</title>"
-			"</head>"
-			"<body>"
-				"<h1>\"MASTER\"</h1>"
-				"<p>\"textbox test\"</p>"
-				"<form action=\"/\" method=\"POST\">"
-					//  changing control0 name
-					"<label for=\"output_ip1\">output_ip1: </label>"
-					"<input type=\"number\" name=\"output_ip1\" placeholder=%s>"
-					"<input type=\"submit\" value=\"submit\"/>"
-				"</form>"
-				"<form action=\"/\" method=\"POST\">"
-					// changing control1 name
-					"<label for=\"output_ip2\">output_ip2: </label>"
-					"<input type=\"number\" name=\"output_ip2\" placeholder=%s>"
-					"<input type=\"submit\" value=\"submit\"/>"
-				"</form>"
-				"<form action=\"/\" method=\"POST\">"
-					// changing control2 name
-					"<label for=\"output_ip3\">output_ip3: </label>"
-					"<input type=\"number\" name=\"output_ip3\" placeholder=%s>"
-					"<input type=\"submit\" value=\"submit\"/>"
-				"</form>"
-				"<form action=\"/\" method=\"POST\">"
-					// changing control3 name
-					"<label for=\"output_ip4\">output_ip4: </label>"
-					"<input type=\"number\" name=\"output_ip4\" placeholder=%s>"
-					"<input type=\"submit\" value=\"submit\"/>"
-				"</form>"
-			"</body>"
-			"</html>",
-			output_ip1,
-			output_ip2,
-			output_ip3,
-			output_ip4);
-	return err;
+	err=sprintf(root_html,
+		"<!DOCTYPE html><html><head><title>ESP32 Config</title></head><body>"
+        "<h1>ESP32 Modbus Configuration</h1>"
+        "<form method=\"POST\" action=\"/\">"
+
+        // Network Interfaces
+        "<section><h2>Network Interfaces</h2>"
+        "<label>IP4 Ethernet:</label><input name=\"ip_eth\" value=\"%s\" /><br>"
+        "<label>IP4 WLAN0:</label><input name=\"ip_wlan0\" value=\"%s\" /><br>"
+        "<label>IP4 WLAN1:</label><input name=\"ip_wlan1\" value=\"%s\" /></section>"
+
+        // Read Section
+        "<section><h2>Read Registers</h2>"
+        "<label>REG0:</label><input name=\"read_reg0\" value=\"%s\" /><br>"
+        "<label>REG1:</label><input name=\"read_reg1\" value=\"%s\" /><br>"
+        "<label>REG2:</label><input name=\"read_reg2\" value=\"%s\" /><br>"
+        "<label>REG3:</label><input name=\"read_reg3\" value=\"%s\" /><br>"
+        "<label>Connect IP:</label><input name=\"read_ip\" value=\"%s\" /><br>"
+        "<label>Bind NETIF IP:</label><input name=\"read_netif_ip\" value=\"%s\" /></section>"
+
+        // Write Section
+        "<section><h2>Write Registers</h2>"
+        "<label>REG0:</label><input name=\"write_reg0\" value=\"%s\" /><br>"
+        "<label>REG1:</label><input name=\"write_reg1\" value=\"%s\" /><br>"
+        "<label>REG2:</label><input name=\"write_reg2\" value=\"%s\" /><br>"
+        "<label>REG3:</label><input name=\"write_reg3\" value=\"%s\" /><br>"
+        "<label>Connect IP:</label><input name=\"write_ip\" value=\"%s\" /><br>"
+        "<label>Bind NETIF IP:</label><input name=\"write_netif_ip\" value=\"%s\" /></section>"
+
+        // Output IP Targets
+        "<section><h2>Output Target IPs</h2>"
+        "<label>Output IP 1:</label><input name=\"output_ip1\" value=\"%s\" /><br>"
+        "<label>Output IP 2:</label><input name=\"output_ip2\" value=\"%s\" /><br>"
+        "<label>Output IP 3:</label><input name=\"output_ip3\" value=\"%s\" /><br>"
+        "<label>Output IP 4:</label><input name=\"output_ip4\" value=\"%s\" /></section>"
+
+        "<input type=\"submit\" value=\"Save\"></form></body></html>",
+
+        ip_eth, ip_wlan0, ip_wlan1,
+        read_reg0, read_reg1, read_reg2, read_reg3, read_ip, read_netif_ip,
+        write_reg0, write_reg1, write_reg2, write_reg3, write_ip, write_netif_ip,
+        output_ip1, output_ip2, output_ip3, output_ip4
+    );
+		
+    return err;
 }
