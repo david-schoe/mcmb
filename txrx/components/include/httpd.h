1f8197500dc678418098c81871ee387926225160
abidrennan <143449277+abidrennan@users.noreply.github.com>
2025-03-27 18:12:52 -0600

    Update httpd.h

--- txrx/components/include/httpd.h
+++ txrx/components/include/httpd.h
@@ -1,19 +1,10 @@
+// removed extern declaration due to redundancy
+
 #ifndef HTTPD_H
 #define HTTPD_H
 
 #include "esp.h"
 #include "html.h"
-
-#define MAXB 64
-#define MAXR 2048
-
-// replace old variables
-extern char output_ip1[MAXB];
-extern char output_ip2[MAXB];
-extern char output_ip3[MAXB];
-extern char output_ip4[MAXB];
-
-extern int err;
 
 httpd_handle_t start_httpd(void);
 
